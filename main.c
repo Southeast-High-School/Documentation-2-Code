884f5486bddea6537c431f8662284dbdd4b60cf2
Southeast-High-School <47824146+Southeast-High-School@users.noreply.github.com>
2019-02-20 09:49:57 -0600

    Documentation 2 '19
    Arm Extends

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-    clear_motor_position_counter(0);
-    while (get_motor_position_counter(0) > -13250)
+    clear_motor_position_counter(0); //clears the motor counter of motor 0
+    while (get_motor_position_counter(0) < 13000) //while motor counter is less than 13000 it will keep doing the loop
     {
-        mav(0,-1500);
+        mav(0, 1500); //rotates the motor so the arm can extend outward 
         msleep(50);
     }
-    ao();    return 0;
+    ao(); //will shut all motors off   
+    return 0;
 }
